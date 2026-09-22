f894c3f3385e486ef421fdb19744199d9cfe4293
Yasifa-alfiatus <yasifa.alfiatus13@smk.belajar.id>
2026-09-22 12:59:29 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan130.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan130 {
+    public static void main(String[] args) {
+        Scanner yasifa = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan Tahun: ");
+        tahun = yasifa.nextInt();
+
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
